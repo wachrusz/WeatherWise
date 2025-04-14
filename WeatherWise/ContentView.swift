36f2a57d401a90576e7edfd3815949c6da41c5a9
Mikhail Vakhrushin <122281679+wachrusz@users.noreply.github.com>
2025-04-14 17:39:51 +0300

    Initial Commit

---
+++ WeatherWise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherWise
+//
+//  Created by Misha Vakhrushin on 14.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
